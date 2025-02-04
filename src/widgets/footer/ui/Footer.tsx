0da47c392e0555cf334d80ff4a77d03df0001098
RomaAbarov <roma.abarov1@gmail.com>
2025-02-04 13:30:14 +0300

    fix: fix footer

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -2,182 +2,90 @@ import Link from "next/link";
 import { Socials } from "@/shared/ui";
 import "./Footer.scss";
 
+const footerLinks = [
+  {
+    title: "Home",
+    links: [
+      "Features",
+      "Blogs",
+      "Resources",
+      "Testimonials",
+      "Contact Us",
+      "Newsletter",
+    ],
+    new: ["Resources"],
+  },
+  {
+    title: "News",
+    links: [
+      "Trending Stories",
+      "Featured Videos",
+      "Technology",
+      "Health",
+      "Politics",
+      "Environment",
+    ],
+  },
+  {
+    title: "Blogs",
+    links: [
+      "Quantum Computing",
+      "AI Ethics",
+      "Space Exploration",
+      "Biotechnology",
+      "Renewable Energy",
+      "Biohacking",
+    ],
+    new: ["Biotechnology"],
+  },
+  {
+    title: "Podcasts",
+    links: [
+      "AI Revolution",
+      "AI Revolution",
+      "TechTalk AI",
+      "AI Conversations",
+    ],
+    new: ["AI Revolution"],
+  },
+  {
+    title: "Resources",
+    links: ["Whitepapers", "Ebooks", "Reports", "Research Papers"],
+  },
+];
+
 export function Footer() {
   return (
     <footer className="footer">
       <div className="footer__inner container">
         <div className="footer__body">
           <nav className="footer__menu">
-            <div className="footer__menu-column">
-              <Link href="/" className="footer__menu-main-link h6">
-                Home
-              </Link>
-              <ul className="footer__menu-list">
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Features
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Blogs
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Resources
-                  </Link>
-                  <span className="badge">New</span>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Testimonials
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Contact Us
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Newsletter
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="footer__menu-column">
-              <Link href="#" className="footer__menu-main-link h6">
-                News
-              </Link>
-              <ul className="footer__menu-list">
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Trending Stories
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Featured Videos
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Technology
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Health
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Politics
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Environment
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="footer__menu-column">
-              <Link href="#" className="footer__menu-main-link h6">
-                Blogs
-              </Link>
-              <ul className="footer__menu-list">
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Quantum Computing
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    AI Ethics
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Space Exploration
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Biotechnology
-                  </Link>
-                  <span className="badge">New</span>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Renewable Energy
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    Biohacking
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="footer__menu-column">
-              <Link href="#" className="footer__menu-main-link h6">
-                Podcasts
-              </Link>
-              <ul className="footer__menu-list">
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    AI Revolution
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    AI Revolution
-                  </Link>
-                  <span className="badge">New</span>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    TechTalk AI
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link">
-                    AI Conversations
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="footer__menu-column">
-              <Link href="#" className="footer__menu-main-link h6">
-                Resources
-              </Link>
-              <ul className="footer__menu-list">
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link button">
-                    <span className="icon icon--yellow-arrow">Whitepapers</span>
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link button">
-                    <span className="icon icon--yellow-arrow">Ebooks</span>
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link button">
-                    <span className="icon icon--yellow-arrow">Reports</span>
-                  </Link>
-                </li>
-                <li className="footer__menu-item">
-                  <Link href="#" className="footer__menu-link button">
-                    <span className="icon icon--yellow-arrow">
-                      Research Papers
-                    </span>
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            {footerLinks.map((link) => (
+              <div key={link.title} className="footer__menu-column">
+                <Link href="#" className="footer__menu-main-link h6">
+                  {link.title}
+                </Link>
+                <ul className="footer__menu-list">
+                  {link.links.map((el, index) => (
+                    <li key={index} className="footer__menu-item">
+                      {link.title === "Resources" ? (
+                        <Link href="#" className="footer__menu-link button">
+                          <span className="icon icon--yellow-arrow">{el}</span>
+                        </Link>
+                      ) : (
+                        <Link href="#" className="footer__menu-link">
+                          {el}
+                        </Link>
+                      )}
+
+                      {link.new?.some((l) => l === el) && (
+                        <span className="badge">New</span>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
           </nav>
         </div>
         <div className="footer__extra">
