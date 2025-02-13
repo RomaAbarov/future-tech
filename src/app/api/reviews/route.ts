9a3090c1af739d793c63b094c2a658e1a564a0a8
RomaAbarov <roma.abarov1@gmail.com>
2025-02-13 12:17:20 +0300

    fix: add route segment config
     make the pages static with updating over time

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import db from "@/../db.json";
 
+export const dynamic = "force-static";
+export const revalidate = 86400;
+
 export async function GET() {
   return NextResponse.json(db.reviews);
 }
--- src/app/news/api/route.ts
+++ src/app/news/api/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import db from "@/../db.json";
 
+export const dynamic = "force-static";
+export const revalidate = 600;
+
 export async function GET(request: NextRequest) {
   const query = request.nextUrl.searchParams.get("_limit");
   const news = query ? db.news.slice(0, +query) : db.news;
